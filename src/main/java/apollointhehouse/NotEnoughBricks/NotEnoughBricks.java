0e49558934abceb03ee55a723d138243e6cd52d9
Apollointhehouse <apollocameronboot@gmail.com>
2023-04-14 11:24:44 +1200

    Renamed files from template

--- src/main/java/apollointhehouse/NotEnoughBricks/NotEnoughBricks.java
+++ src/main/java/apollointhehouse/NotEnoughBricks/NotEnoughBricks.java
@@ -1,16 +1,16 @@
-package turniplabs.examplemod;
+package apollointhehouse.NotEnoughBricks;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "examplemod";
+public class NotEnoughBricks implements ModInitializer {
+    public static final String MOD_ID = "notenoughbricks";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("NotEnoughBricks initialized.");
     }
 }
